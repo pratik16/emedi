f6d01a8d27a9156a2f5af3c6e814e3bd0a44b767
suresh <sureshkanzariya94@gmail.com>
2021-11-25 20:43:26 +0530

    [fix-issues] User login and logout with remove token.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/users/user.service';
 import { FormBuilder, Validators, NgForm } from '@angular/forms';
 import { ValidationService } from 'src/app/services/validation.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,28 +13,22 @@ export class LoginComponent implements OnInit {
 
   loading_register: boolean = false;
   registerError: any = null;
-  registerForm: any = {};
+  loginForm: any = {};
   countries: any = [];
-
+  flag : boolean = false;
   constructor(
     private userService: UserService,
     private formBuilder: FormBuilder,
+    private router : Router
   ) { 
 
     
   }
 
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
-			'first_name': ['', Validators.required],
-			'last_name': ['', [Validators.required]],
+    this.loginForm = this.formBuilder.group({
 			'email': ['', [Validators.required, ValidationService.emailValidator]],
-			'password': ['', [Validators.required]],
-			'password_confirmation': ['', [Validators.required]],
-			'phone': ['', [Validators.required]],
-      'gender': ['', []],
-      'term': ['', [Validators.required]],
-      'country_id': ['', [Validators.required]],
+			'password': ['', [Validators.required]]			
     });
     
     this.userService.config().subscribe((data:any) => {
@@ -43,25 +38,35 @@ export class LoginComponent implements OnInit {
 
   doRegister(e:any) {
     e.preventDefault();
+    console.log("this.loginForm.value  ===?", this.loginForm.value);
     this.registerError = false;
-		if (this.registerForm.valid) {
+		if (this.loginForm.valid) {
+      this.flag = true;
 			this.loading_register = true;
-			let args = this.registerForm.value;
+			let args = this.loginForm.value;
+      console.log("args  ===?", args);
 			//args.password_confirmation = args.password;
-		/*	this.userService.register(args).subscribe((data: any) => {
-        let success = data.json();
+			this.userService.login(args).subscribe((data: any) => {
+        let success = data;
         console.log(success);
-				/*this.user = success.data;
-				this.auth.setUser(this.user, false);
-				this.loading_register = false;
-				this.setActiveForm('login');
+        if(success && success.token)
+        {
+          localStorage.setItem("token", success.token);
+          this.router.navigate(["/"]);
+          this.userService.isLoggedIn = true;
+          this.flag = false;
+        }
+				// this.user = success.data;
+				// this.auth.setUser(this.user, false);
+				// this.loading_register = false;
+				// this.setActiveForm('login');
 
 			}, (error:any) => { 
         let e = error.error;
         this.registerError = e;
 				this.loading_register = false;
 
-			})*/
+			})
     }
     
     return false;
